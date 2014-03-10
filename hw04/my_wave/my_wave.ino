645a70e552ec0c0a43108a0225fa283ad69803bd
Nathan Lintz <nlintz@gmail.com>
2014-03-10 11:09:41 -0400

    rational completed

--- hw04/my_wave/my_wave.ino
+++ hw04/my_wave/my_wave.ino
@@ -16,6 +16,7 @@ void setup() {
 
   pinMode(buttonPin1, INPUT_PULLUP);  
   pinMode(buttonPin2, INPUT_PULLUP);  
+  attachInterrupt(0, playSound, FALLING);
 
   pinMode(ledPin, OUTPUT);
 
@@ -26,29 +27,74 @@ void setup() {
   }
 }
 
-void writeByte(int x) {
-  int pin;
-  for (pin=13; pin>=6; pin--) {
-    digitalWrite(pin, x&1);
-    x >>= 1;
+void bitTable(int table[], int x)
+{
+  int i;
+  for (i=0; i<8; i++){
+    table[i] = (x & (1 << i)) ? 1 : 0;
   }
+}
+
+int getPortD(int table[])
+{
+  int i;
+  int total = 0;
+  total += table[7] << 6;
+  total += table[6] << 7;
+  total+= (1 << buttonPin1);
+  return total;
+}
+
+int getPortB(int bitTable[])
+{
+  int i;
+  int total = 0;
+  total += bitTable[0] << 5;
+  total += bitTable[1] << 4;
+  total += bitTable[2] << 3;
+  total += bitTable[3] << 2;
+  total += bitTable[4] << 1;
+  total += bitTable[5];
+  return total;
+}
+
+void writeByte(int x) {
+  
+  int table[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  bitTable(table, x);
+  int d = getPortD(table);
+  int b = getPortB(table);
+  PORTD = d;
+  PORTB = b;
 }
 
 int low = 36;
 int high = 255;
 int stride = 5;
 int counter = low;
+int play = true;
 
 void loop() {
-  int button1 = digitalRead(buttonPin1);
-  if (button1) return;
+//  int button1 = digitalRead(buttonPin1);
+//  if (button1) return;
 
   counter += stride;
   if (counter > high) {
     counter = low;
   }
+  
+  if play
+    writeByte(counter);
+}
 
-  // write to the digital pins  
-  writeByte(counter);
+void playSound()
+{
+  static unsigned long last_millis = 0;
+  unsigned long m = millis();
+  if (m - last_millis > 200) { //Debounce the Interrupt
+    Serial.println("trigger sound");
+    play = !play;
+    last_millis = m;
+  }
 }
 
--- lecture13/rational.c
+++ lecture13/rational.c
@@ -12,20 +12,25 @@ typedef struct {
 // Returns a new Rational with the given numer and denom.
 // If unable to allocate, prints an error message and exits.
 Rational *make_rational(int numer, int denom) {
-    // FILL THIS IN
-    return NULL;
+    Rational *rational = malloc(sizeof(Rational));
+    rational->numer = numer;
+    rational->denom = denom;
+    return rational;
 }
 
 // Computes a floating-point approximation of a Rational.
 double rational_to_double(Rational *rational) {
-    // FILL THIS IN
-    return 0.0;
+    double rational_dbl;
+    rational_dbl = (double)(rational->numer)/(double)(rational->denom);
+    return rational_dbl;
 }
 
 // Multiplies two rational numbers; returns a new Rational.
 Rational *mult_rational(Rational *r1, Rational *r2) {
-    // FILL THIS IN
-    return NULL;
+    int new_numer = r1->numer * r2->numer;
+    int new_denom = r1->denom * r2->denom;
+    Rational *new_rational = make_rational(new_numer, new_denom);
+    return new_rational;
 }
 
 // Frees a Rational.
